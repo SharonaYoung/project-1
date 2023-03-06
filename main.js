0785c5710878730dce82432d08466715503943ce
Sharona Young <msrona806@gmail.com>
2023-03-06 14:54:45 -0500

    api responding successfully

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 // api: https://api.giphy.com/v1/gifs/search?api_key=vg6WXDw9SY1PiOQGjPyZw2r4nytNngc0&q=cats&limit=12&offset=0&rating=g&lang=en
+const mainEl = document.querySelector('main');
 const API_key = "vg6WXDw9SY1PiOQGjPyZw2r4nytNngc0";
 
 // ajax request to api
@@ -6,19 +7,22 @@ async function getGif() {
   try{
     const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${API_key}&q=cats&limit=12&offset=0&rating=g&lang=en`);
   const data = await response.json();
-
-  console.log(data.meta);
   console.log(data.data);
+  // call the function to show returned gifs
+  renderGifs(data.data);
   }
   catch (error) {
     console.log(error);
   }
 }
 getGif();
-
+// function to show gifs in the DOM
 function renderGifs(gifs) {
-  const gifTemplate = gifs.map(gif => (
-    console.log(gif.title)
-  ))
-}
-renderGifs();
+  const gifTemplate = gifs.map(gif => ( 
+    `
+  //     <img src=${gif.images.fixed_height.url} alt="gif image" />
+  //   `
+  // remove comma after each image    
+  )).join(' ');
+  mainEl.innerHTML = gifTemplate;
+};
